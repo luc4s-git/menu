88c91b783b81b0e5eab54c8c39d747864fb2ba24
luc4s-git <lucassantos4332@hotmail.com>
2023-11-30 19:28:49 -0300

    menu item header

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,7 +3,10 @@ export default function MenuItem({ id, title, category, price, img, desc }) {
     <div className="menu-item">
       <img src={img} alt={title} className="img" />
       <div className="item-info">
-        <h5>{title}</h5>
+        <header>
+          <h5>{title}</h5>
+          <span className="item-price">${price}</span>
+        </header>
       </div>
     </div>
   );
